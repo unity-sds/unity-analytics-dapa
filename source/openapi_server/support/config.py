a5eb184ffc05fc7396fd98da8631973086a170c5
xing <xing>
2023-07-14 10:47:28 -0700

    add remote proxy (more)

---
+++ source/openapi_server/support/config.py
@@ -0,0 +1,11 @@
+PROCESSOR_DICT = {
+    "time-series:area": [],
+    "hovmoller": [],
+    "time-average:yearly": []
+}
+
+#SDAP_SERVER_URL_PREFIX = "https://coverage.ceos.org/nexus/longitudeTimeHofMoellerSpark"
+
+#https://55j2i6e704.execute-api.us-west-2.amazonaws.com/sdap-dev/timeSeriesSpark?ds=MUR25-JPL-L4-GLOB-v4.2_analysed_sst&minLon=-77.0&minLat=35.0&maxLon=-70.0&maxLat=42.0&startTime=2018-01-01T00:00:00Z&endTime=2018-12-31T00:00:00Z
+SDAP_SERVER_URL_PREFIX = "https://55j2i6e704.execute-api.us-west-2.amazonaws.com/sdap-dev/timeSeriesSpark"
+
