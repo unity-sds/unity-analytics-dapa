83991fbf51a1c2ab2f7f836d600ffb3ed009a7d6
z xing <xing@jpl.nasa.gov>
2024-02-04 22:14:18 -0800

    dapa service update

--- source/openapi_server/support/config.py
+++ source/openapi_server/support/config.py
@@ -1,11 +1,11 @@
+import os
+
 PROCESSOR_DICT = {
     "time-series:area": [],
     "hovmoller": [],
     "time-average:yearly": []
 }
 
-#SDAP_SERVER_URL_PREFIX = "https://coverage.ceos.org/nexus/longitudeTimeHofMoellerSpark"
-
-#https://55j2i6e704.execute-api.us-west-2.amazonaws.com/sdap-dev/timeSeriesSpark?ds=MUR25-JPL-L4-GLOB-v4.2_analysed_sst&minLon=-77.0&minLat=35.0&maxLon=-70.0&maxLat=42.0&startTime=2018-01-01T00:00:00Z&endTime=2018-12-31T00:00:00Z
-SDAP_SERVER_URL_PREFIX = "https://55j2i6e704.execute-api.us-west-2.amazonaws.com/sdap-dev/timeSeriesSpark"
-
+SDAP_SERVER_URL_PREFIX = "http://127.0.0.1/timeSeriesSpark"
+if os.environ.get("SDAP_SERVER_URL_PREFIX") is not None:
+    SDAP_SERVER_URL_PREFIX = os.environ["SDAP_SERVER_URL_PREFIX"]
